f272dda7bdcde30eda164a4de0976b193bc9c939
Arthur Prado De Fazio <Arthur_Prado_De_Fazio@MacBook-Pro-Arthur-Prado-De-Fazio.local>
2023-02-15 20:19:51 -0300

    OK

--- CityReader.cs
+++ CityReader.cs
@@ -5,7 +5,7 @@ namespace AsyncWeather
 	{
 		public static async Task ReadCityList()
 		{
-			await Task.Delay(100000);
+			await Task.Delay(20000);
 			Console.WriteLine("Cidades lidas");
 		}
 	}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        await WeatherService.GetWeatherAsync();
-        await CityReader.ReadCityList();
+        Task pegarTempo = WeatherService.GetWeatherAsync();
+        Task lerCidades = CityReader.ReadCityList();
+        await pegarTempo;
+        await lerCidades;
+        _ = Task.WhenAll(new Task[] { pegarTempo, lerCidades });
     }
 }
 
